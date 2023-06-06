7d82570fbef7feb2c8555011947e343c6dd674c1
FacundoTorres <torresfdev@gmail.com>
2023-06-06 00:45:51 -0300

    se creo CRUD funcional

--- src/controllers/ProductManager.js
+++ src/controllers/ProductManager.js
@@ -15,6 +15,11 @@ class ProductManager{
         await fs.writeFile(this.path, JSON.stringify(product))
     }
 
+    exist = async(id) =>{
+        let products = await this.readProducts()
+        return products.find(prod => prod.id === id)
+    }
+    
     addProducts = async(product) =>{
         let productsOld=  await this.readProducts()
         product.id = nanoid(5)
@@ -25,6 +30,33 @@ class ProductManager{
 
     getProducts= async() => {
         return await this.readProducts()
+    }
+
+    getProductsById= async(id) => {
+        let productById=await this.exist(id)
+        if(!productById) return"product not found"
+        return productById
+    }
+
+    deleteProducts= async(id)=>{
+        let products = await this.readProducts()
+        let existProducts =products.some(prod => prod.id === id)
+        if(existProducts){
+            let filterProducts= products.filter(prod => prod.id != id)
+            await this.writeProducts(filterProducts)
+            return "product deleted"
+        }
+        return "the product you want to delete does not exist"
+    }
+
+    updateProduct= async(id, product) =>{
+        let productById=await this.exist(id)
+        if(!productById) return"product not found"
+        await this.deleteProducts(id)
+        let productOld = await this.readProducts()
+        let products = [{...product, id : id}, ...productOld]
+        await this.writeProducts(products)
+        return "updated product"
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -13,10 +13,27 @@ app.use(express.urlencoded({extended: true}));
 app.get("/products", async (req,res)=>{
    res.send(await product.getProducts())
 })
+app.get("/products/:id", async (req,res)=>{
+    let id= req.params.id
+    res.send(await product.getProductsById(id))
+ })
 
 app.post("/products", async (req,res)=>{
     let newProduct = req.body
     res.send(await product.addProducts(newProduct))
+})
+
+app.put("/products/:id", async (req,res)=>{
+    let id= req.params.id
+    let updateProduct = req.body
+    res.send(await product.updateProduct(id, updateProduct))
+
+
+})
+
+app.delete("/products/:id", async (req,res)=>{
+    let id= req.params.id
+    res.send(await product.deleteProducts(id))
 })
 
 app.listen(port,()=>{
