17a95551b933d86ce13c78c506f95e49b508808c
Cecilia Vaccarezza <torresfdev@gmail.com>
2023-06-19 20:01:46 -0300

    se soluciono problema con websocket

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import express from "express";
-import handlebars from "express-handlebars"
+import handlebars from "express-handlebars";
 import productRouter from "./router/product.routes.js";
 import CartRouter from "./router/carts.routes.js";
 import ProductManager from "./controllers/ProductManager.js";
-import {Server} from 'socket.io'
+import { Server } from "socket.io";
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -13,37 +13,36 @@ const app = express();
 const port = 8080;
 const product = new ProductManager();
 
-
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "./public")));
 
-app.engine('handlebars', handlebars.engine())
-app.set('views','./src/views')
-app.set('view engine', 'handlebars')
+app.engine("handlebars", handlebars.engine());
+app.set("views", "./src/views");
+app.set("view engine", "handlebars");
 
-app.use("/api/products", productRouter)
-app.use("/api/cart", CartRouter)
+/* app.use("/api/products", productRouter)
+app.use("/api/cart", CartRouter) */
 
-app.get('/home', async (req,res)=>{
-    let allproducts = await product.getProducts()
-    res.render('home',{
-        products : allproducts
-    })
-})
+app.get("/home", async (req, res) => {
+  let allproducts = await product.getProducts();
+  res.render("home", {
+    products: allproducts,
+  });
+});
 
-app.get('/realTimeProducts', async (req,res)=>{
-    let allproducts = await product.getProducts()
-    res.render('realTimeProducts',{
-        products : allproducts
-    })
-})
+app.get("/realTimeProducts", async (req, res) => {
+  let allproducts = await product.getProducts();
+  res.render("realTimeProducts", {
+    products: allproducts,
+  });
+});
 
+const serverHttp = app.listen(port, () => {
+  console.log(`servidor ON ${port}`);
+});
+const io = new Server(serverHttp);
 
-
-const serverHttp= app.listen(port,()=>{console.log(`servidor ON ${port}`)})  
-const io= new Server(serverHttp)
-
-io.on('connection',()=>{
-    console.log("se realizo una conexion")
-})
+io.on("connection", () => {
+  console.log("se realizo una conexion");
+});
